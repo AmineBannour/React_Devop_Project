9e4d956c961df9839cf8f6ead0a7c7d34891d6f7
Med Amine Bannour <bannouramine40@gmail.com>
2025-11-26 15:07:58 +0100

    Fix smoke test to target backend service name

--- backend/smoke-test-backend.sh
+++ backend/smoke-test-backend.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 mkdir -p artifacts
 
-# Try 10 times before failing
+# Try 10 times because backend may take time to start
 for i in {1..10}; do
-    STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://mern-backend:5000)
-    echo "Attempt $i: status = $STATUS"
-    
-    if [ "$STATUS" -eq 200 ]; then
-        echo "BACKEND OK" > artifacts/backend-smoke.txt
-        exit 0
-    fi
-    
-    sleep 3
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://backend:5000/)
+  echo "Attempt $i: status = $STATUS"
+
+  if [ "$STATUS" -eq 200 ]; then
+    echo "BACKEND OK" > artifacts/backend-smoke.txt
+    exit 0
+  fi
+
+  sleep 3
 done
 
-echo "BACKEND FAILED ($STATUS)" > artifacts/backend-smoke.txt
+echo "BACKEND FAILED" > artifacts/backend-smoke.txt
 exit 1
